db5196dfb31cabe4f1be34f7127ab7e5bc337807
peanut-lover <jihoson94@gmail.com>
2023-03-16 16:41:39 +0900

    Add Tooltop(only data)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [milliseconds, setMilliseconds] = useState(Date.now());
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setMilliseconds(Date.now());
+    }, 100);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <div className="AnalogTimer">
+    <div className="Container">
+      <div className="AnalogTimer" title={String(milliseconds)}>
         <div className="Origin"></div>
         <div className="Hand HourHand"></div>
         <div className="Hand MinHand"></div>
